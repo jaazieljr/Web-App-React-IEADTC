100c4311162aff006200573d80110859b4a13d87
jaazieljr <jaaziel.dev@gmail.com>
2022-10-28 15:10:42 -0300

    front nav

--- web/src/components/Nav/index.jsx
+++ web/src/components/Nav/index.jsx
@@ -2,21 +2,32 @@ import { Link } from 'react-router-dom'
 import React from "react"
 import './styles.css'
 import Logo from './assets/logo.svg'
+import Lupa from './assets/lupa.svg'
+import Menu from './assets/menu.svg'
 function Nav() {
     return (
         <nav>
-            <img className="logo" src={Logo} alt="" />
-            <ul className="nav-list">
-                <li>
-                <Link to='/Mensagens' style={{textDecoration: 'none'}}> <span>Mensagens</span> </Link>
-                </li>
-                <li>
-                <Link to='/Contato' style={{textDecoration: 'none'}}> <span>Contato</span> </Link>
-                </li>
-                <li>
-                <Link to='/Login' style={{textDecoration: 'none'}}><span>Obreiros</span></Link>
-                </li>
-            </ul>
+            <Link to='/' style={{ textDecoration: 'none' }}><img className="logo" src={Logo} alt="" /></Link>
+            
+            
+                <ul className="nav-list">
+                    <li>
+                        <Link to='/Mensagens' style={{ textDecoration: 'none' }}> <span>Mensagens</span> </Link>
+                    </li>
+                    <li>
+                        <Link to='/Contato' style={{ textDecoration: 'none' }}> <span>Contato</span> </Link>
+                    </li>
+                    <li>
+                        <Link to='/Login' style={{ textDecoration: 'none' }}><span>Adephos</span></Link>
+                    </li>
+                    <li>
+                        <Link to='/Login' style={{ textDecoration: 'none' }}><span><img className="lupa" src={Lupa} alt="" /></span></Link>
+                    </li>
+                </ul>
+            <div>
+                <button><img className="logo" src={Menu} alt="" /></button>
+            </div>
+
         </nav>
     );
 }
